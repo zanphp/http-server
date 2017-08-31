df0423e6d5bc191c509919d77cd677d9e79b9663
xiaohengjin <xiaohengjin@youzan.com>
2017-08-31 11:05:15 +0800

    Http Server instance

--- META-INF/service.php
+++ META-INF/service.php
@@ -1,3 +1,11 @@
 <?php
 
+use ZanPHP\Support\Di;
+
+$container = \ZanPHP\Container\Container::getInstance();
+
+$container->bind("ServerBase.HttpServer", function ($_, $args) {
+    return Di::make(\ZanPHP\HttpServer\Server::class, [$args[0], $args[1]]);
+});
+
 return [];
