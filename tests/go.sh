6161e9a2e82377c3f175d717565f0b67e9c5e566
huye <huye@youzan.com>
2017-10-13 16:39:21 +0800

    fix

--- tests/go.sh
+++ tests/go.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-path=`pwd`/vendor
+path=`pwd`
+vendor=$path/vendor
+composer=$path/composer.json
 case "$1" in
     start)
-        if [ ! -d $path ]; then
-            ln -s ../vendor/ $path
+        if [ ! -d $vendor ]; then
+            ln -s ../vendor/ $vendor
+        fi
+        if [ ! -f $composer ]; then
+            ln -s ../composer.json $composer
         fi
         echo "start http server"
         php mockServer/HttpServer.php >/dev/null 2>&1 &
@@ -11,7 +16,7 @@ case "$1" in
     ;;
     stop)
         echo "stop http server"
-        httpServer=$(ps -ef|grep 'php HttpServer.php'|grep -v "grep"|awk -F " " '{print $2}'|sort)
+        httpServer=$(ps -ef|grep 'php mockServer/HttpServer.php'|grep -v "grep"|awk -F " " '{print $2}'|sort)
         kill -9 $httpServer
     ;;
     *)
